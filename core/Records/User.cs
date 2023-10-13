9df7b381a76e8e843b6a89e58bc48b06db82dd06
Geordanis B. Vega <gbanoaol@gmail.com>
2023-10-13 15:58:20 -0300

    adding age field

--- core/Records/User.cs
+++ core/Records/User.cs
@@ -6,10 +6,11 @@ namespace core.Records
 		public int Id { get; set; }
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
+		public int Age { get; set; }
 
 		public override string ToString()
         {
-			return $"Id: {Id} | FirstName: {FirstName} | LastName: {LastName}!!!";
+			return $"Id: {Id} | FirstName: {FirstName} | LastName: {LastName} | Age: {Age}";
         }
     }
 }
